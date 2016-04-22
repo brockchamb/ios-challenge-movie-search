0b036217b5aa5b61f0938823c62e915d09a4e6f5
Brock Chambers <brockchamb@gmail.com>
2016-04-22 15:39:57 -0600

    Mostly done

--- MovieSearch/NetworkController.swift
+++ MovieSearch/NetworkController.swift
@@ -16,10 +16,6 @@ class NetworkController {
         return NSURL(string: "http://api.themoviedb.org/3/search/movie?query=\(modifiedSearchTerm)&api_key=68a8779098e355ac704c413416c684af")!
     }
     
-    static func popularURL() -> NSURL {
-        return NSURL(string: "http://api.themoviedb.org/3/movie/popular?api_key=68a8779098e355ac704c413416c684af")!
-    }
-    
     static func dataAtURL(url:NSURL, completion:(resultData:NSData?) -> Void){
         
         let session = NSURLSession.sharedSession()
